1d789d66ffb86735ca6417e1b785a093ed67293f
banchet18 <banjarac6@gmail.com>
2024-01-15 21:05:11 +0530

    c

--- tradeverse-project/src/App.jsx
+++ tradeverse-project/src/App.jsx
@@ -7,9 +7,21 @@ function App() {
         <div>
           <input type="text" />
           <div>
-            <div></div>
-            <div></div>
+            <div>icon</div>
+            <div>profile</div>
             <div>Username</div>
+          </div>
+        </div>
+        {/* {first div completed} */}
+        <div>
+          <div>
+            <h4>Current licence</h4>
+            <h4>USD/CAD</h4>
+            <h4>AUD/USD</h4>
+          </div>
+          <div>
+            <h3>Welcome, Username</h3>
+            <h5>Good to see you again!</h5>
           </div>
         </div>
       </div>
